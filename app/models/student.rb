dfac0897b0b8cbb6fe18284c775f26efc433c270
Michael Quaas <michael.quaas@betterplace.org>
2015-09-19 00:21:00 +0200

    some navigation testing

--- app/models/student.rb
+++ app/models/student.rb
@@ -10,16 +10,28 @@ class Student < ActiveRecord::Base
   end
 
   rails_admin do
+      # navigation_label 'Praktikum'
+      parent Course
+
+
 
     list do
+      filters [:course, :education_subject]
       field :first_name
       field :last_name
-      field :year
+      field(:year) { column_width 30 }
       field(:email, :email)
       include_fields :education_subject, :course, :city
       # field(:created_at) { date_format :short }
       # field(:updated_at) { date_format :short }
     end
+
+    # show do
+    #   group(:default)
+    #   group(:address)
+    #   field(:name) { group :default }
+    #   field(:city) { group :address }
+    # end
 
   end
 end
