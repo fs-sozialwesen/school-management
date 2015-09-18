9aab91c7b863e1467a85488b8134e05d31576bd8
Michael Quaas <michael.quaas@betterplace.org>
2015-09-19 00:19:20 +0200

    locales

--- app/models/mentor.rb
+++ app/models/mentor.rb
@@ -3,6 +3,11 @@ class Mentor < ActiveRecord::Base
   belongs_to :carrier,     inverse_of: :mentors
   belongs_to :institution, inverse_of: :mentors
 
+  def name
+    "#{first_name} #{last_name}"
+  end
+
+
   rails_admin do
     navigation_label 'Praktikum'
     parent Institution
