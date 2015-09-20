037df54235a1ef2dfd127b350c1e52b584f52246
Michael Quaas <michael.quaas@betterplace.org>
2015-09-20 18:19:10 +0200

    reformat factories

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,16 +2,16 @@ FactoryGirl.define do
 
   FIRST_NAMES = %w(Sabine Carola Marianne Thomas Mike Claudia Klaus Conrad Max Johannes Florian
                       Anne Björn Franziska Sarah Tobias Yvonne)
-  LAST_NAMES  = %w(Meier Müller Schulze Schmidt Schmitt Meyer Varoufakis Steinbrecher Landsknecht Albrecht)
+  LAST_NAMES = %w(Meier Müller Schulze Schmidt Schmitt Meyer Varoufakis Steinbrecher Landsknecht Albrecht)
   EMAIL_HOSTS = %w(gmx.net web.de yahoo.com gmail.com)
-  CITYS       = %w(Aschersleben Ballenstedt Quedlinburg Halberstadt Hettstedt Wernigerode)
+  CITYS = %w(Aschersleben Ballenstedt Quedlinburg Halberstadt Hettstedt Wernigerode)
 
   sequence(:first_name) { FIRST_NAMES.sample }
-  sequence(:last_name)  { LAST_NAMES.sample }
-  sequence(:city)       { CITYS.sample }
-  sequence(:street)     { "Sehr Lange Str. #{(rand*180).to_i}" }
-  sequence(:zip)        { rand.to_s[2..6] }
-  sequence(:email)      { |n| "info_#{n}@#{EMAIL_HOSTS.sample}" }
-  sequence(:phone)      { '0' + rand.to_s[2..(7..11).to_a.sample] }
+  sequence(:last_name) { LAST_NAMES.sample }
+  sequence(:city) { CITYS.sample }
+  sequence(:street) { "Sehr Lange Str. #{(rand*180).to_i}" }
+  sequence(:zip) { rand.to_s[2..6] }
+  sequence(:email) { |n| "info_#{n}@#{EMAIL_HOSTS.sample}" }
+  sequence(:phone) { '0' + rand.to_s[2..(7..11).to_a.sample] }
 
 end
