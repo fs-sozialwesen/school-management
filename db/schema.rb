a3afa5ad9a752d44b1d0ec9105e8eb6e2dd363e4
Michael Quaas <michael.quaas@betterplace.org>
2015-09-28 20:47:31 +0200

    schema.rb updated

--- db/schema.rb
+++ db/schema.rb
@@ -101,20 +101,6 @@ ActiveRecord::Schema.define(version: 20150920010845) do
   add_index "internships", ["mentor_id"], name: "index_internships_on_mentor_id", using: :btree
   add_index "internships", ["student_id"], name: "index_internships_on_student_id", using: :btree
 
-  create_table "lessons", force: :cascade do |t|
-    t.integer  "time_table_id"
-    t.string   "subject"
-    t.integer  "teacher_id"
-    t.string   "block"
-    t.string   "room"
-    t.text     "comments"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-  end
-
-  add_index "lessons", ["teacher_id"], name: "index_lessons_on_teacher_id", using: :btree
-  add_index "lessons", ["time_table_id"], name: "index_lessons_on_time_table_id", using: :btree
-
   create_table "mentors", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
@@ -129,36 +115,6 @@ ActiveRecord::Schema.define(version: 20150920010845) do
 
   add_index "mentors", ["carrier_id"], name: "index_mentors_on_carrier_id", using: :btree
   add_index "mentors", ["institution_id"], name: "index_mentors_on_institution_id", using: :btree
-
-  create_table "practice_guides", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "carrier_id"
-    t.integer  "institution_id"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "comments"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "practice_guides", ["carrier_id"], name: "index_practice_guides_on_carrier_id", using: :btree
-  add_index "practice_guides", ["institution_id"], name: "index_practice_guides_on_institution_id", using: :btree
-
-  create_table "practice_leaders", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.integer  "carrier_id"
-    t.integer  "institution_id"
-    t.string   "email"
-    t.string   "phone"
-    t.text     "comments"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
-  end
-
-  add_index "practice_leaders", ["carrier_id"], name: "index_practice_leaders_on_carrier_id", using: :btree
-  add_index "practice_leaders", ["institution_id"], name: "index_practice_leaders_on_institution_id", using: :btree
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
@@ -189,16 +145,6 @@ ActiveRecord::Schema.define(version: 20150920010845) do
     t.string   "city"
   end
 
-  create_table "time_tables", force: :cascade do |t|
-    t.integer  "course_id"
-    t.date     "start_date"
-    t.date     "end_date"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "time_tables", ["course_id"], name: "index_time_tables_on_course_id", using: :btree
-
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
@@ -218,12 +164,9 @@ ActiveRecord::Schema.define(version: 20150920010845) do
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
     t.integer  "role"
-    t.integer  "loginable_id"
-    t.string   "loginable_type"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["loginable_id"], name: "index_users_on_loginable_id", using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   create_table "versions", force: :cascade do |t|
@@ -244,13 +187,6 @@ ActiveRecord::Schema.define(version: 20150920010845) do
   add_foreign_key "internships", "internship_positions"
   add_foreign_key "internships", "mentors"
   add_foreign_key "internships", "students"
-  add_foreign_key "lessons", "teachers"
-  add_foreign_key "lessons", "time_tables"
   add_foreign_key "mentors", "carriers"
   add_foreign_key "mentors", "institutions"
-  add_foreign_key "practice_guides", "carriers"
-  add_foreign_key "practice_guides", "institutions"
-  add_foreign_key "practice_leaders", "carriers"
-  add_foreign_key "practice_leaders", "institutions"
-  add_foreign_key "time_tables", "courses"
 end
