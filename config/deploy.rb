fd5e36aba91f76b261273b49d7f3f123550dc34f
Michael Quaas <michael.quaas@betterplace.org>
2015-10-10 14:06:00 -0700

    capistrano puma options for staging
    restart the server automatically

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,6 +21,27 @@ set :default_stage, "staging"
 set :linked_files, %w{config/database.yml}
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+# puma options with defaults
+# set :puma_user, fetch(:user)
+# set :puma_rackup, -> { File.join(current_path, 'config.ru') }
+# set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+# set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
+# set :puma_bind, "unix://#{shared_path}/tmp/sockets/puma.sock"    #accept array for multi-bind
+# for staging, needs to be adapted for production:
+set :puma_bind, "tcp://0.0.0.0:9292"    #accept array for multi-bind
+# set :puma_default_control_app, "unix://#{shared_path}/tmp/sockets/pumactl.sock"
+# set :puma_conf, "#{shared_path}/puma.rb"
+# set :puma_access_log, "#{shared_path}/log/puma_access.log"
+# set :puma_error_log, "#{shared_path}/log/puma_error.log"
+# set :puma_role, :app
+# set :puma_env, fetch(:rack_env, fetch(:rails_env, 'production'))
+# set :puma_threads, [0, 16]
+# set :puma_workers, 0
+# set :puma_worker_timeout, nil
+# set :puma_init_active_record, false
+# set :puma_preload_app, true
+# set :nginx_use_ssl, false
+
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
