039d441d93e2986e44d2a5c398e25b993852dd9c
Michael Quaas <michael.quaas@betterplace.org>
2015-10-29 02:30:51 -0700

    show only internship positions from current year

--- app/controllers/internship_positions_controller.rb
+++ app/controllers/internship_positions_controller.rb
@@ -2,7 +2,7 @@ class InternshipPositionsController < ApplicationController
 
   def index
     student = current_user.person
-    @internship_positions = InternshipPosition.where(education_subject: student.education_subject).all
+    @internship_positions = InternshipPosition.where(education_subject: student.education_subject, year: Date.today.year).all
   end
 
   def show
