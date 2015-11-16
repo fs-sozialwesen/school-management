c8a40b59b06d01ef7363a63f0ac2fb45b0d0960a
Michael Quaas <michael.quaas@betterplace.org>
2015-11-15 18:01:52 -0800

    update internship_position search

--- app/controllers/internship_positions_controller.rb
+++ app/controllers/internship_positions_controller.rb
@@ -1,13 +1,28 @@
 class InternshipPositionsController < ApplicationController
 
   def index
+
+    if params[:submit] == 'clear'
+      %i(city accommodation by_email).each { |filter| params.delete filter}
+    end
+
     student = current_user.as_student
+    internship_offers = InternshipOffer
+    internship_offers = internship_offers.with_accommodation if params[:accommodation] == 'yes'
+    internship_offers = internship_offers.without_accommodation if params[:accommodation] == 'no'
+    internship_offers = internship_offers.by_email if params[:by_email]
+    internship_offers = internship_offers.where(city: params[:city]) if params[:city].present?
+    ids = internship_offers.pluck(:id)
+    # filter = { education_subject: student.education_subject, internship_offers: {id: ids}}
+    filter = { education_subject: EducationSubject.last, internship_offers: {id: ids}}
+
     @internship_positions = InternshipPosition.
       includes(internship_offer: :organisation).
-      where(
-        education_subject: student.education_subject
-      ).
+      where(filter).
+      order('internship_offers.city').
       all
+
+    @cities = @internship_positions.map{ |ip| ip.internship_offer.city }.compact.uniq.sort
   end
 
   def show
