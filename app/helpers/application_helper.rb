5af3433a411d4231ba3c54916a76bedf7a564c5a
Michael Quaas <michael.quaas@betterplace.org>
2015-11-15 18:00:18 -0800

    view helper for boolean values

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def bool_icon(val)
+    css_class = 'glyphicon glyphicon-'
+    css_class += val ? 'ok' : 'minus'
+    content_tag :span, nil, class: css_class
+  end
 end
