9b37759f23dbf7722d59425a501b0acd2a01641e
Michael Quaas <michael.quaas@betterplace.org>
2015-11-17 18:47:29 -0800

    the missing stuff

--- app/models/internship_position.rb
+++ app/models/internship_position.rb
@@ -1,40 +1,41 @@
-class InternshipOffer < ActiveRecord::Base
+class InternshipPosition < ActiveRecord::Base
 
-  belongs_to :organisation, inverse_of: :internship_offers
-  has_many :internship_positions, inverse_of: :internship_offer, dependent: :destroy
+  belongs_to :organisation, inverse_of: :internship_positions
+  belongs_to :education_subject, required: true
 
   serialize :address,  Address
   serialize :contact,  Contact
   serialize :housing,  Housing
   serialize :applying, Applying
-  serialize :positions, Positions
+  # serialize :positions, Positions
 
   # delegate :by_phone,  :by_email,  :by_mail,  :documents,  to: :application_options, prefix: :application
   # delegate :by_phone=, :by_email=, :by_mail=, :documents=, to: :application_options, prefix: :application
 
   scope :applying_by, -> (via = nil)     { via.present? ? where('applying @> ?', Applying.by(via)) : all }
   scope :housing,     -> (housing = nil) { (housing.in? [true, false]) ? where('housing @> ?', Housing.provided(housing)) : all }
-  scope :by_city,     -> (city) do
-    main_city  = { city: city }.to_json
-    other_city = [ { address: { city: city } } ].to_json
-    where('((address @> ?) OR (positions @> ?))', main_city, other_city)
-  end
+  scope :by_city,     -> (city = nil)    { city.blank? ? all : where('internship_positions.address @> ?', {city: city}.to_json) }
+  # scope :by_city,     -> (city) do
+  #   main_city  = { city: city }.to_json
+  #   other_city = [ { address: { city: city } } ].to_json
+  #   where('((address @> ?) OR (positions @> ?))', main_city, other_city)
+  # end
   # scope :by_pos_city, -> (city = nil)    { city.blank? ? all : where('positions @> ?', [{address:{city: city}}].to_json) } # positions @> '[{"address":{"city": "Magdeburg"}}]'
 
-  # InternshipOffer.where('positions ? :education_subject_id', education_subject_id: '2').count
-  # InternshipOffer.where('positions ?& array[:keys]', keys: ['2']).count
+  # where('positions ? :education_subject_id', education_subject_id: '2').count
+  # where('positions ?& array[:keys]', keys: ['2']).count
 
-  # InternshipOffer.where("application_options ->>'documents' ILIKE '%leben%'" ).count
+  # where("application_options ->>'documents' ILIKE '%leben%'" ).count
 
-  accepts_nested_attributes_for :internship_positions
+  # accepts_nested_attributes_for :internship_positions
 
-  def positions_sum
-    internship_positions.sum :number_of_positions
-  end
-
-  def education_subjects
-    internship_positions.map(&:education_subject_count).join(', ')
-  end
+  # def positions_sum
+  #   internship_positions.sum :number_of_positions
+  # end
+  #
+  # def education_subjects
+  #   internship_positions.map(&:education_subject_count).join(', ')
+  # end
 
   rails_admin do
     # parent Organisation
@@ -46,67 +47,69 @@ class InternshipOffer < ActiveRecord::Base
     # configure :application_options do
     #   formatted_value { bindings[:object].application_options.by_options.join(', ').html_safe }
     # end
-
+    configure :work_area, :enum do
+      enum { ['Kindertagesstätten', 'Hort', 'Heim', 'Tagesgruppe', 'offene Kinder- und Jugendarbeit', 'Psychiatrie', 'Schule'] }
+    end
+    # configure(:city) { pretty_value { bindings[:object].address.city } }
     list do
-      configure(:city) { pretty_value { bindings[:object].address.city } }
       sort_by :organisation
       # field :id
+      field :education_subject
       field :name, :self_link
       field :organisation
       # field :application_options
       # field :application_documents
-      field :city
-      field :description
+      # field :city
+      # field :description
       # field :accommodation
-      field :education_subjects
-      field :positions_sum
+      field :positions_count
     end
 
-    show do
-      field :organisation
-      field :name
-      field :city
-      field :description
-      field :education_subjects
-      field :positions_sum
-
-      # field :application_options
-      # field :application_documents
-    end
-
-    edit do
-      group :default do
-        field :name
-        field :description
-        field :internship_positions
-      end
-      group :contact do
-        label 'Kontakt'
-        field :contact_person
-        field :email
-        field :phone
-        field :fax
-        field :homepage
-      end
-      group :organisation do
-        field :organisation do
-          inline_add false
-          inline_edit false
-        end
-      end
-      group :address do
-        label 'Adresse'
-        field :street
-        field :zip
-        field :city
-      end
-      # group :application do
-      #   field :application_by_phone
-      #   field :application_by_email
-      #   field :application_by_mail
-      #   field :application_documents
-      # end
-    end
+    # show do
+    #   field :organisation
+    #   field :name
+    #   field :city
+    #   field :description
+    #   field :education_subjects
+    #   field :positions_sum
+    #
+    #   # field :application_options
+    #   # field :application_documents
+    # end
+    #
+    # edit do
+    #   group :default do
+    #     field :name
+    #     field :description
+    #     field :internship_positions
+    #   end
+    #   group :contact do
+    #     label 'Kontakt'
+    #     field :contact_person
+    #     field :email
+    #     field :phone
+    #     field :fax
+    #     field :homepage
+    #   end
+    #   group :organisation do
+    #     field :organisation do
+    #       inline_add false
+    #       inline_edit false
+    #     end
+    #   end
+    #   group :address do
+    #     label 'Adresse'
+    #     field :street
+    #     field :zip
+    #     field :city
+    #   end
+    #   # group :application do
+    #   #   field :application_by_phone
+    #   #   field :application_by_email
+    #   #   field :application_by_mail
+    #   #   field :application_documents
+    #   # end
+    # end
   end
 
 end
--- db/migrate/20151101010022_create_internship_positions.rb
+++ db/migrate/20151101010022_create_internship_positions.rb
@@ -1,6 +1,6 @@
-class CreateInternshipOffers < ActiveRecord::Migration
+class CreateInternshipPositions < ActiveRecord::Migration
   def change
-    create_table :internship_offers do |t|
+    create_table :internship_positions do |t|
       t.string :name
       t.references :organisation,      index: true, foreign_key: true
       t.references :education_subject, index: true, foreign_key: true
@@ -10,13 +10,12 @@ class CreateInternshipOffers < ActiveRecord::Migration
 
       t.date :start_date
       t.date :end_date
-      t.integer :number_of_positions, default: 1
+      t.integer :positions_count, default: 1
 
       t.jsonb :address,   default: '{}'
       t.jsonb :contact,   default: '{}'
       t.jsonb :housing,   default: '{}'
       t.jsonb :applying,  default: '{}'
-      t.jsonb :positions, default: '[]'
 
       t.timestamps null: false
     end
