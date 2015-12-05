b42833de574d538ab595d333a3278444e9eb9c4c
Michael Quaas <michael.quaas@betterplace.org>
2015-12-04 20:45:04 -0800

    remove restart test tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,27 +48,5 @@ set :nginx_sites_enabled_path, "/etc/nginx/conf.d/sites-enabled"
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
-namespace :deploy do
 
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      execute :touch, release_path.join('tmp/restart.txt')
-      # execute :echo, '$(pwd)'
-      # execute :echo, release_path
-    end
-  end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-  after :finishing, 'deploy:cleanup'
-
-end
