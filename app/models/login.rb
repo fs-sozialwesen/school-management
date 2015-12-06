f0ad95d0925f4da12168972f83b9e17929a01855
Michael Quaas <michael.quaas@betterplace.org>
2015-12-06 11:18:12 -0800

    login timeout

--- app/models/login.rb
+++ app/models/login.rb
@@ -5,7 +5,7 @@ class Login < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable
 
   def display_name
     email
