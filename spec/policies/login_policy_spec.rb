6704cd407383f47410ec64ad4ac7a5ce566302ca
Michael Quaas <michael.quaas@betterplace.org>
2016-01-25 07:58:47 -0500

    fix login policy spec

--- spec/policies/login_policy_spec.rb
+++ spec/policies/login_policy_spec.rb
@@ -4,20 +4,20 @@ describe LoginPolicy do
   let (:current_login) { FactoryGirl.build_stubbed :login }
   let (:current_user)  { current_login.build_person }
   let (:other_login) { FactoryGirl.build_stubbed :login }
-  let (:admin_login) { FactoryGirl.build_stubbed :login }
-  let (:admin_user) do
-    p = admin_login.build_person
-    p.create_as_admin
+  let (:manager_login) { FactoryGirl.build_stubbed :login }
+  let (:manager_user) do
+    p = manager_login.build_person
+    p.create_as_manager
     p
   end
 
   permissions :index? do
-    it "denies access if not an admin" do
+    it "denies access if not an manager" do
       expect(LoginPolicy).not_to permit(current_login.build_person)
     end
-    it "allows access for an admin" do
-      # admin.build_person.build_as_admin.save
-      expect(LoginPolicy).to permit(admin_user)
+    it "allows access for an manager" do
+      # manager.build_person.build_as_manager.save
+      expect(LoginPolicy).to permit(manager_user)
     end
   end
 
@@ -28,17 +28,17 @@ describe LoginPolicy do
     it "allows you to see your own profile" do
       expect(subject).to permit(current_user, current_login)
     end
-    it "allows an admin to see any profile" do
-      expect(subject).to permit(admin_user)
+    it "allows an manager to see any profile" do
+      expect(subject).to permit(manager_user)
     end
   end
 
   permissions :update? do
-    it "prevents updates if not an admin" do
+    it "prevents updates if not an manager" do
       expect(subject).not_to permit(current_user)
     end
-    it "allows an admin to make updates" do
-      expect(subject).to permit(admin_user)
+    it "allows an manager to make updates" do
+      expect(subject).to permit(manager_user)
     end
   end
 
@@ -46,8 +46,8 @@ describe LoginPolicy do
     it "prevents deleting yourself" do
       expect(subject).not_to permit(current_user, current_login)
     end
-    it "allows an admin to delete any login" do
-      expect(subject).to permit(admin_user, other_login)
+    it "allows an manager to delete any login" do
+      expect(subject).to permit(manager_user, other_login)
     end
   end
 
