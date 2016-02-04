ccea8ceb2021afb6326f641bdec83bf258e543f5
Michael Quaas <michael.quaas@betterplace.org>
2016-02-03 22:26:09 -0500

    feature spec candidate form parts

--- spec/features/people/manage_candidates_spec.rb
+++ spec/features/people/manage_candidates_spec.rb
@@ -13,12 +13,12 @@ feature 'Manage candidates', :devise do
     expect(page).to have_content 'Meyer'
   end
 
-  scenario 'edit candidate' do
+  scenario 'edit person data' do
     sign_in_as_manager
 
     create_candidate Vorname: 'Frank', Nachname: 'Meyer', Anrede: 'Herr'
     click_row_with 'Frank'
-    within('.panel', match: :first) { click_on 'Bearbeiten' }
+    within('.panel.person') { click_on 'Bearbeiten' }
     select 'Herr',            from: 'Anrede'
     fill_in 'Vorname',        with: ''
     fill_in 'Geburtsdatum',   with: '1.1.1996'
@@ -35,7 +35,57 @@ feature 'Manage candidates', :devise do
     expect(page).to have_content 'Rodriges Gonzales'
   end
 
-  scenario 'delete student' do
+  scenario 'edit documents' do
+    sign_in_as_manager
+
+    create_candidate Vorname: 'Frank', Nachname: 'Meyer', Anrede: 'Herr'
+    click_row_with 'Frank'
+    within('.panel.documents') { click_on 'Bearbeiten' }
+    # save_and_open_page
+    select 'Grade1', from: :Schulabschluss
+    select 'Prof2', from: :Berufsausbildung
+    click_on 'Speichern'
+    expect(page).to have_content 'Bewerber(in) gespeichert'
+  end
+
+  scenario 'edit interview' do
+    sign_in_as_manager
+
+    create_candidate Vorname: 'Frank', Nachname: 'Meyer', Anrede: 'Herr'
+    click_row_with 'Frank'
+    within('.panel.interview') { click_on 'Bearbeiten' }
+    fill_in 'Datum', with: '01.07.2016'
+    check 'eingeladen'
+    select 'Zusage', from: 'Rückmeldung'
+    select 'Wiederholen', from: 'Ergebnis'
+    click_on 'Speichern'
+    expect(page).to have_content 'Bewerber(in) gespeichert'
+    expect(page).to have_content '01.07.2016'
+  end
+
+  scenario 'edit contracts' do
+    sign_in_as_manager
+
+    create_candidate Vorname: 'Frank', Nachname: 'Meyer', Anrede: 'Herr'
+    click_row_with 'Frank'
+    within('.panel.contracts') { click_on 'Bearbeiten' }
+    within('.panel.education-contract') do
+      fill_in 'Versand am', with: '01.08.2016'
+      fill_in 'zurück',     with: '03.08.2016'
+    end
+    within('.panel.internship-contract') do
+      fill_in 'Versand am', with: '01.09.2016'
+      fill_in 'zurück',     with: '03.09.2016'
+    end
+    click_on 'Speichern'
+    expect(page).to have_content 'Bewerber(in) gespeichert'
+    expect(page).to have_content '01.08.2016'
+    expect(page).to have_content '03.08.2016'
+    expect(page).to have_content '01.09.2016'
+    expect(page).to have_content '03.09.2016'
+  end
+
+  scenario 'delete candidate' do
     sign_in_as_manager
 
     create_candidate Vorname: 'Frank', Nachname: 'Meyer', Anrede: 'Herr'
