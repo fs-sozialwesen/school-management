40083c29e3fad5b7de1f0383b2eb27f74090ec03
Michael Quaas <michael.quaas@betterplace.org>
2016-02-03 22:26:25 -0500

    session helper cleanup

--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -1,19 +1,10 @@
 module Features
   module SessionHelpers
 
-    def sign_up_with(email, password, confirmation)
-      visit new_login_registration_path
-      fill_in Login.human_attribute_name(:email),    with: email
-      fill_in Login.human_attribute_name(:password), with: password
-      fill_in Login.human_attribute_name(:password), :with => confirmation
-      click_button I18n.t('devise.sessions.new.sign_out')
-    end
-
     def signin(email, password)
       visit new_login_session_path
       fill_in Login.human_attribute_name(:email),    with: email
       fill_in Login.human_attribute_name(:password), with: password
-      # save_and_open_page
       click_button I18n.t('devise.sessions.new.sign_in')
     end
 
@@ -23,16 +14,16 @@ module Features
       signin(manager.login.email, '12341234')
     end
 
-    def signin_login(login)
-      signin login.email, login.password
-    end
+    # def signin_login(login)
+    #   signin login.email, login.password
+    # end
 
-    def signin_person(person)
-      signin_login person.login
-    end
+    # def signin_person(person)
+    #   signin_login person.login
+    # end
 
-    def signin_role(role)
-      signin_login role.person.login
-    end
+    # def signin_role(role)
+    #   signin_login role.person.login
+    # end
   end
 end
