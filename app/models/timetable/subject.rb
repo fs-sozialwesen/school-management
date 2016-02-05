55c2d10a5645064cfe5a7310d6ca391f5779ca18
Michael Quaas <michael.quaas@betterplace.org>
2016-02-05 12:14:22 -0500

    validate name on subject

--- app/models/timetable/subject.rb
+++ app/models/timetable/subject.rb
@@ -1,7 +1,7 @@
 class Timetable::Subject < ActiveRecord::Base
+  # has_many :lessons, inverse_of: :subject
 
-  has_many :lessons, inverse_of: :subject
+  validates :name, presence: true
 
   has_paper_trail
-
 end
