909cf52ff3f8bf40735090dab5434bd003b6bc39
Michael Quaas <michael.quaas@betterplace.org>
2016-02-25 17:21:34 -0300

    load env vars from yml file

---
+++ config/initializers/_environment_variables.rb
@@ -0,0 +1,8 @@
+module SchoolManagement
+  class Application < Rails::Application
+    config.before_configuration do
+      env_file = Rails.root.join('config', 'environment_variables.yml').to_s
+      YAML.load_file(env_file).each { |key, value| ENV[key.to_s] = value }
+    end
+  end
+end
