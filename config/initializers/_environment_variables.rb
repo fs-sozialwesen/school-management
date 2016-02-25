ab97983ebc4db0aecfa8db6a1c172a76ebe79bee
Michael Quaas <michael.quaas@betterplace.org>
2016-02-25 17:24:44 -0300

    do not load env vars in test (codeship)

--- config/initializers/_environment_variables.rb
+++ config/initializers/_environment_variables.rb
@@ -3,6 +3,6 @@ module SchoolManagement
     config.before_configuration do
       env_file = Rails.root.join('config', 'environment_variables.yml').to_s
       YAML.load_file(env_file).each { |key, value| ENV[key.to_s] = value }
-    end
+    end unless Rails.env.test?
   end
 end
