9968ec8073529fd9c58f2bb7814f9be700e689f8
Michael Quaas <michael.quaas@betterplace.org>
2016-03-01 19:32:36 -0300

    Teacher sees timetable docs as pdf download

--- app/controllers/teacher_time_tables_controller.rb
+++ app/controllers/teacher_time_tables_controller.rb
@@ -9,10 +9,11 @@ class TeacherTimeTablesController < ApplicationController
 
   def show
     authorize TimeTable
-    @date = Date.parse params[:id]
-    @lessons = current_user.as_teacher.lessons_at @date
-    @weeks = current_user.as_teacher.time_table_dates
-    @time_blocks = TimeBlock.all
+    @date                = Date.parse params[:id]
+    @lessons             = current_user.as_teacher.lessons_at @date
+    @weeks               = current_user.as_teacher.time_table_dates
+    @time_blocks         = TimeBlock.all
+    @timetable_documents = TimetableDocument.order(:year, :start_date).all
   end
 
 end
