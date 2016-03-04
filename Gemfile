960ab2d0049e3fcbc9b4c6cda30dde5a0a3223bf
Michael Quaas <michael.quaas@betterplace.org>
2016-03-04 18:35:23 -0300

    Installing and configuring airbrake/errbit

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'select2-rails'
 gem 'acts-as-taggable-on', '~> 3.4'
 gem 'axlsx_rails', '0.3.0'
 gem 'paperclip'
+gem 'airbrake', '~> 5.0'
 
 group :development, :test do
   gem 'byebug'
---
+++ config/initializers/errbit.rb
@@ -0,0 +1,5 @@
+Airbrake.configure do |config|
+  config.host = ENV['ERRBIT_HOST']
+  config.project_id = true
+  config.project_key = ENV['ERRBIT_KEY']
+end if ENV['ERRBIT_HOST'].present? and ENV['ERRBIT_KEY'].present?
