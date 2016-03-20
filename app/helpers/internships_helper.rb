aab03cfe478ba9cfae5284cae037149025fe8c94
Michael Quaas <michael.quaas@betterplace.org>
2016-03-20 14:03:12 -0300

    select internship position and mentor after selecting organisation

--- app/helpers/internships_helper.rb
+++ app/helpers/internships_helper.rb
@@ -1,2 +1,15 @@
 module InternshipsHelper
+  def json_for_select(organisations)
+    json_organisations = {}
+    organisations.each do |o|
+      json_organisations[o.id] = { name: o.name }
+
+      ips = json_organisations[o.id][:ips] = {}
+      o.internship_positions.each { |ip| ips[ip.id] = ip.name }
+
+      mentors = json_organisations[o.id][:mentors] = {}
+      o.mentors.each { |mentor| mentors[mentor.id] = mentor.name }
+    end
+    json_organisations
+  end
 end
--- app/models/internship.rb
+++ app/models/internship.rb
@@ -1,6 +1,7 @@
 class Internship < ActiveRecord::Base
   belongs_to :student, required: true, inverse_of: :internships
   belongs_to :internship_position, required: true
+  has_one :organisation, through: :internship_position
   belongs_to :mentor
 
   validates :start_date, :end_date, presence: true
