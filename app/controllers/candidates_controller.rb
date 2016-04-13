9392bc9bf8b789749c7047e564a116f1d56cffa9
Michael Quaas <michael.quaas@betterplace.org>
2016-04-13 11:17:50 +0200

    Candidate: add career_changer and rank attributes

--- app/controllers/candidates_controller.rb
+++ app/controllers/candidates_controller.rb
@@ -49,7 +49,7 @@ class CandidatesController < ApplicationController
 
   def accept
     @candidate.accept!
-    redirect_to candidate_url(@candidate), notice: 'Bewerber aufgenommen!'
+    redirect_to @candidate, notice: 'Bewerber aufgenommen!'
   end
 
   def reject
@@ -70,7 +70,7 @@ class CandidatesController < ApplicationController
                  date notes education_subject year police_certificate internships cancel_date
                  internships_proved education_contract_sent education_contract_received
                  internship_contract_sent internship_contract_received cancel_reason status
-                 debit_mandate contract_notes ) +
+                 debit_mandate contract_notes career_changer rank) +
                 [
                   address: %i(street zip city),
                   contact: %i(email phone mobile),
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411193422) do
+ActiveRecord::Schema.define(version: 20160413085711) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,8 @@ ActiveRecord::Schema.define(version: 20160411193422) do
     t.string   "place_of_birth"
     t.jsonb    "address",                      default: {}
     t.jsonb    "contact",                      default: {}
+    t.boolean  "career_changer",               default: false
+    t.integer  "rank"
   end
 
   add_index "candidates", ["person_id"], name: "index_candidates_on_person_id", using: :btree
