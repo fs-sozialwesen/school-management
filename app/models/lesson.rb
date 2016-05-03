bfaa0c8bfe4ba34358fbe100e1e040bf43a2feaf
Michael Quaas <michael.quaas@betterplace.org>
2016-05-03 13:06:49 +0200

    lesson model spec

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -7,12 +7,7 @@ class Lesson < ActiveRecord::Base
 
   validates :weekday, presence: true, uniqueness: { scope: %i(time_table_id time_block_id) }
 
-  def empty?
-    teacher_id.blank? or subject_id.blank?
-  end
-
   def teacher_name
-    return '' unless teacher.present?
     [teacher.first_name[0...1], teacher.last_name].join '. '
   end
 
--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,4 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Lesson, type: :model do
+  describe '#teacher_name' do
+    it 'gives the shortened name' do
+      teacher = Teacher.new person_attributes: { first_name: 'John', last_name: 'Terry' }
+      subject.teacher = teacher
+
+      expect(subject.teacher_name).to eql 'J. Terry'
+    end
+  end
 end
