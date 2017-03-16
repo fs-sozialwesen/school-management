8f24ce05e1a54f37cafc97fbc5a1338da7bd058a
Michael Quaas <michael.quaas@betterplace.org>
2017-03-16 10:11:07 +0100

    fix spec helpers load

--- spec/support/helpers.rb
+++ spec/support/helpers.rb
@@ -1,4 +1,10 @@
 require 'support/helpers/session_helpers'
+require 'support/helpers/people_helpers'
+require 'support/helpers/course_helpers'
+require 'support/helpers/timetable_helpers'
+require 'support/helpers/internship_helpers'
+require 'support/helpers/clickable_table_helpers'
+
 RSpec.configure do |config|
   config.include Features::SessionHelpers, type: :feature
   config.include Features::PeopleHelpers, type: :feature
