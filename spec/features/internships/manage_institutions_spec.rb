26c89fb0219a685f1fafbfab391ce61cd888621d
Michael Quaas <michael.quaas@betterplace.org>
2017-05-10 00:30:20 +0200

    test test coverage changes on code climate

--- spec/features/internships/manage_institutions_spec.rb
+++ spec/features/internships/manage_institutions_spec.rb
@@ -3,35 +3,35 @@ feature 'Manage institutions', :devise do
 #   I want to be able to add, find, edit and delete institutions
 #   So I can provide internship positions to the students
 
-  scenario 'create institution' do
-    sign_in_as_manager
-
-    create_institution 'Einrichtung1'
-  end
-
-  scenario 'edit institution' do
-    sign_in_as_manager
-    given_institution 'Einrichtung1'
-
-    click_on 'Einrichtungen'
-    click_row_with 'Einrichtung1'
-    click_on 'Bearbeiten'
-    fill_in :Name, with: ''
-    click_on 'Speichern'
-    expect(page).to have_content('Einrichtung konnte nicht gespeichert werden')
-    fill_in :Name, with: 'Einrichtung 42'
-    click_on 'Speichern'
-    expect(page).to have_content('Einrichtung gespeichert')
-    expect(page).to have_content('Einrichtung 42')
-  end
-
-  scenario 'delete institution' do
-    sign_in_as_manager
-    given_institution 'Einrichtung1'
-
-    click_on 'Einrichtungen'
-    click_row_with 'Einrichtung1'
-    click_on 'Löschen'
-    expect(page).to have_content('Einrichtung gelöscht')
-  end
+  # scenario 'create institution' do
+  #   sign_in_as_manager
+  #
+  #   create_institution 'Einrichtung1'
+  # end
+  #
+  # scenario 'edit institution' do
+  #   sign_in_as_manager
+  #   given_institution 'Einrichtung1'
+  #
+  #   click_on 'Einrichtungen'
+  #   click_row_with 'Einrichtung1'
+  #   click_on 'Bearbeiten'
+  #   fill_in :Name, with: ''
+  #   click_on 'Speichern'
+  #   expect(page).to have_content('Einrichtung konnte nicht gespeichert werden')
+  #   fill_in :Name, with: 'Einrichtung 42'
+  #   click_on 'Speichern'
+  #   expect(page).to have_content('Einrichtung gespeichert')
+  #   expect(page).to have_content('Einrichtung 42')
+  # end
+  #
+  # scenario 'delete institution' do
+  #   sign_in_as_manager
+  #   given_institution 'Einrichtung1'
+  #
+  #   click_on 'Einrichtungen'
+  #   click_row_with 'Einrichtung1'
+  #   click_on 'Löschen'
+  #   expect(page).to have_content('Einrichtung gelöscht')
+  # end
 end
