7efd07876a745a00b08015b93623b5ca784094bd
Michael Quaas <michael@qua.as>
2017-09-02 20:14:35 +0200

    Fix old migration

--- db/migrate/20160407100606_activate_all_students.rb
+++ db/migrate/20160407100606_activate_all_students.rb
@@ -1,7 +1,8 @@
 class ActivateAllStudents < ActiveRecord::Migration
   def up
     Student.update_all active: true
-    Course.find_by(name: 'Dropouts').students.update_all active: false
+    dropouts = Course.find_by(name: 'Dropouts')
+    dropouts.students.update_all active: false if dropouts
   end
 
   def down
