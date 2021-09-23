ebdb05a13138b966c9061da01d93ad288355bab8
Michael Quaas <michael@qua.as>
2021-09-23 10:16:08 +0200

    authorize contract termination

--- app/controllers/student/contract_terminations_controller.rb
+++ app/controllers/student/contract_terminations_controller.rb
@@ -1,10 +1,15 @@
 class Student::ContractTerminationsController < ApplicationController
+  before_action :authenticate_login!
+  after_action :verify_authorized
+
   def new
+    authorize Student, :terminate_contract?
     @student = Student.find params[:student_id]
     @contract_termination = @student.build_contract_termination
   end
 
   def create
+    authorize Student, :terminate_contract?
     @student = Student.find params[:student_id]
     @contract_termination = @student.build_contract_termination(
       params.require(:student_contract_termination).permit(:date, :notes, :terminated_by)
